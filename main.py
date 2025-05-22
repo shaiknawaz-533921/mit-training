1769f2d13ba9bb592f43e71df41446065678b372
BTMogit <mogit.bt@mastechdigital.com>
2025-05-22 15:51:44 +0530

    Added Multiplicatio Agent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from GreetAgent import GreetAgent
 from BirthdayAgent import BirthdayAgent
+from MultiplicationAgent import MultiplicationAgent
 
 if __name__ == "__main__":
     greet_agent = GreetAgent()
@@ -7,3 +8,6 @@ if __name__ == "__main__":
 
     birthday_agent = BirthdayAgent()
     print(birthday_agent.process_request("Bob"))  # Output: Happy Birthday, Bob! 🎉
+    
+    multiplication_agent = MultiplicationAgent()
+    print(multiplication_agent.process_request(2, 3, 4))
